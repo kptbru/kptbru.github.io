3d8ec616adc1b014f9fde36304193728fa597ca8
kptbru <86619727+kptbru@users.noreply.github.com>
2021-10-14 12:44:14 +0300

    Update articles.js

--- js/articles.js
+++ js/articles.js
@@ -118,3 +118,19 @@ let content3 = downLinks.map(linke => `
     `);
 
 ground.innerHTML = content3.join('');
+
+function addLink() {
+  var body_element = document.getElementsByTagName('body')[0];
+  var selection;
+  selection = window.getSelection();
+  var pagelink = "<br /><br /> Источник: <a href='"+document.location.href+"'>"+document.location.href+"</a><br />";
+  var copytext = selection + pagelink;
+  var newdiv = document.createElement('div');
+  newdiv.style.position='absolute';
+  newdiv.style.left='-99999px';
+  body_element.appendChild(newdiv);
+  newdiv.innerHTML = copytext;
+  selection.selectAllChildren(newdiv);
+  window.setTimeout(function() {body_element.removeChild(newdiv)},0);
+}
+document.oncopy = addLink;
