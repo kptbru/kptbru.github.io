3abe041a6c83c17c0745ef0f00c45d1ac928895b
kptbru <86619727+kptbru@users.noreply.github.com>
2021-10-14 12:45:11 +0300

    Update videos.js

--- js/videos.js
+++ js/videos.js
@@ -408,3 +408,19 @@ let content10 = other.map(block => `
     `);
 
 others.innerHTML = content10.join('');
+
+function addLink() {
+  var body_element = document.getElementsByTagName('body')[0];
+  var selection;
+  selection = window.getSelection();
+  var pagelink = "<br /><br /> Источник: <a href='"+document.location.href+"'>"+document.location.href+"</a><br />";
+  var copytext = selection + pagelink;
+  var newdiv = document.createElement('div');
+  newdiv.style.position='absolute';
+  newdiv.style.left='-99999px';
+  body_element.appendChild(newdiv);
+  newdiv.innerHTML = copytext;
+  selection.selectAllChildren(newdiv);
+  window.setTimeout(function() {body_element.removeChild(newdiv)},0);
+}
+document.oncopy = addLink;
