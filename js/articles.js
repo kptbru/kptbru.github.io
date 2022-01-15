2ac93771b91ad2af103f8b81baf54d5e73c21f25
Morozov Pavel <filipencop@mail.ru>
2022-01-15 20:58:37 +0200

    major changes in design

--- js/articles.js
+++ js/articles.js
@@ -61,35 +61,7 @@ let blocks = [
     }
 ]
 
-let menuLinks = [
-  {
-    main: 'index.html',
-    about: 'about.html',
-    posts: 'posts.html',
-    articles: 'articles.html',
-    serbia: 'https://www.instagram.com/kptb_srb/?hl=ru',
-    kirgiz: 'https://www.instagram.com/kekp_kyrgyzstan/?utm_medium=copy_link',
-    ukraine: 'https://www.instagram.com/mptb_ukraina/?hl=ru',
-    germany: 'https://www.instagram.com/kpam_de/',
-    tvorchestvo: 'https://www.instagram.com/souz_proletarskogo_tvorchestva/',
-    italy: 'https://www.instagram.com/kptb_italia/',
-    newzealand: 'https://www.instagram.com/kptb_nz_aotearoa/',
-    videos: 'videos.html',
-    joint: 'join.html',
-  }
-]
-
-let downLinks = [
-  {
-    name: 'НАШИ КОНТАКТЫ',
-    youtube: 'https://www.youtube.com/channel/UCvO2vXE3fLN8W5QiRsMFP6g/featured',
-    telegram: 'https://t.me/kptb_org',
-    instagram: 'https://www.instagram.com/kptb_russia/?igshid=tzl9uephxohe',
-    vk: 'https://vk.com/club180844773',
-  }
-]
-
-const containe = document.querySelector('.containe');
+const container = document.querySelector('.container');
     let content1 = blocks.map(block => `
         <div class="item">
             <img src="${block.photo}\.jpg" class="photo" alt="photo">
@@ -102,52 +74,8 @@ const containe = document.querySelector('.containe');
         </div>
     `);
 
-containe.innerHTML = content1.join('');
+container.innerHTML = content1.join('');
 
-
-const menu = document.querySelector('.menu');
-
-let content2 = menuLinks.map(link => `
-    <ul>
-        <li class="logo"><a href="${link.main}"><img src="images/kptbIcon.jpg" class="kptbIcon" alt="kptbIcon"></a></li>
-        <li><a href="${link.main}">Главная страница</a></li>
-        <li><a href="${link.about}">О нас</a></li>
-        <li><a href="#">Новости</a>
-            <ul lass="dropdown">
-                <li><a href="${link.posts}">Посты</a></li>
-                <li><a href="${link.articles}">Статьи</a></li>
-            </ul>
-        </li>
-        <li><a href="#">Наши отделы</a>
-            <ul class="dropdown">
-                <li><a href="${link.serbia}">КПТБ Сербии</a></li>
-                <li><a href="${link.kirgiz}">КПТБ Киргизии</a></li>
-                <li><a href="${link.ukraine}">МПТБ Украины</a></li>
-                <li><a href="${link.germany}">КПТБ Германии</a></li>
-                <li><a href="${link.tvorchestvo}">Союз Пролетарского Творчества</a></li>
-                <li><a href="${link.italy}">КПТБ Италии</a></li>
-                <li><a href="${link.newzealand}">КПТБ Новой Зеландии</a></li>
-            </ul>
-        </li>
-        <li><a href="${link.videos}">Наши видео</a></li>
-        <li><a href="${link.joint}">Присоединиться к нам</a></li>
-    </ul>
-    `);
-
-menu.innerHTML = content2.join('');
-
-const ground = document.querySelector('.ground');
-let content3 = downLinks.map(linke => `
-    <ul>
-        <li class="nazwa">${linke.name}</li>
-        <li><a href="${linke.youtube}">Наш Youtube</a></li>
-        <li><a href="${linke.telegram}">Наш Telegram</a></li>
-        <li><a href="${linke.instagram}">Наш Instagram</a></li>
-        <li><a href="${linke.vk}">Наша группа Вконтакте</a></li>
-    </ul>
-    `);
-
-ground.innerHTML = content3.join('');
 
 function addLink() {
   var body_element = document.getElementsByTagName('body')[0];
--- js/menus.js
+++ js/menus.js
@@ -16,24 +16,14 @@ let menuLinks = [
     }
 ]
 
-let downLinks = [
-    {
-      name: 'НАШИ КОНТАКТЫ',
-      youtube: 'https://www.youtube.com/channel/UCvO2vXE3fLN8W5QiRsMFP6g/featured',
-      telegram: 'https://t.me/kptb_org',
-      instagram: 'https://www.instagram.com/kptb_russia/?igshid=tzl9uephxohe',
-      vk: 'https://vk.com/club180844773',
-    }
-]
-
 const menu = document.querySelector('.menu');
-    let content1 = menuLinks.map(link => `
-    <ul>
-        <li class="logo"><a href="${link.main}"><img src="images/kptbIcon.jpg" class="kptbIcon" alt="kptbIcon"></a></li>
+let topLinksContent = menuLinks.map(link => `
+    <a class="brand kptbIcon" href="${link.main}"><img src="images/kptbIcon.jpg" alt="kptbIcon"></a>
+    <ul class="navigation-list">
         <li><a href="${link.main}">Главная страница</a></li>
         <li><a href="${link.about}">О нас</a></li>
         <li><a href="#">Новости</a>
-            <ul lass="dropdown">
+            <ul class="dropdown">
                 <li><a href="${link.posts}">Посты</a></li>
                 <li><a href="${link.articles}">Статьи</a></li>
             </ul>
@@ -52,12 +42,23 @@ const menu = document.querySelector('.menu');
         <li><a href="${link.videos}">Наши видео</a></li>
         <li><a href="${link.joint}">Присоединиться к нам</a></li>
     </ul>
-    `);
+`);
 
-menu.innerHTML = content1.join('');
+menu.innerHTML = topLinksContent.join('');
+
+
+let bottomLinks = [
+    {
+      name: 'НАШИ КОНТАКТЫ',
+      youtube: 'https://www.youtube.com/channel/UCvO2vXE3fLN8W5QiRsMFP6g/featured',
+      telegram: 'https://t.me/kptb_org',
+      instagram: 'https://www.instagram.com/kptb_russia/?igshid=tzl9uephxohe',
+      vk: 'https://vk.com/club180844773',
+    }
+]
 
 const ground = document.querySelector('.ground');
-let content2 = downLinks.map(linke => `
+let bottomLinksContent = bottomLinks.map(linke => `
     <ul>
         <li class="nazwa">${linke.name}</li>
         <li><a href="${linke.youtube}">Наш Youtube</a></li>
@@ -67,7 +68,7 @@ let content2 = downLinks.map(linke => `
     </ul>
     `);
 
-ground.innerHTML = content2.join('');
+ground.innerHTML = bottomLinksContent.join('');
 
 function addLink() {
   var body_element = document.getElementsByTagName('body')[0];
