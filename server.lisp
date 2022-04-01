21c9a90e4a889489cc1e9f51a8f85a972745bccb
Morozov Pavel <filipencop@mail.ru>
2022-04-02 00:36:27 +0300

    Перевел посты

--- server.lisp
+++
@@ -1,8 +0,0 @@
-(ql:quickload :hunchentoot)
-
-(defvar *acceptor* (make-instance 'hunchentoot:easy-acceptor
-                                  :document-root "~/devel/web/kptbnew/"
-                                  :port 4242))
-
-(unless (hunchentoot:started-p *acceptor*)
-  (hunchentoot:start *acceptor*))
